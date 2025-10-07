7c1bc008ff3acfa474e7d9084dbaa6715602dd86
Koalka48 <temagrigorev2013@gmail.com>
2025-10-07 13:10:29 +0300

    lesson 3.4 done

--- src/core/models.py
+++ src/core/models.py
@@ -23,3 +23,15 @@ class DogUserModel(AbstractUser, BaseModel):
 
     def __str__(self):
         return self.username
+
+class BarkModel(BaseModel):
+    message = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(DogUserModel, on_delete=models.CASCADE, related_name='barks')
+    
+    class Meta:
+        verbose_name = "Bark"
+        verbose_name_plural = "Barks"
+
+def __str__(self):
+        return f"{self.user.username} - {self.message[:20]}..."
